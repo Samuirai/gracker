b4e128714966da54cc3639f4a539c0b9822a9183
filip <haase.filip@googlemail.com>
2011-10-20 09:11:23 +0200

    Fixed Home Controller Mail Stuff

--- grails-app/controllers/org/gracker/HomeController.groovy
+++ grails-app/controllers/org/gracker/HomeController.groovy
@@ -3,10 +3,5 @@ package org.gracker
 class HomeController {
 
     def index = { 
-		sendMail {     
-		  to "filip.haase@googlemail.com"     
-		  subject "Hello Flip"     
-		  body 'Hier war jemand auf Home ;)' 
-		}
 	}
 }
