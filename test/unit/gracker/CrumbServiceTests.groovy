6ba2fc3ce82a459f9fbd3adc6f1c57daca7e4b41
Fabian Faessler <fabi@fabif.de>
2011-10-27 09:01:42 +0200

    still fix stuff

---
+++ test/unit/gracker/CrumbServiceTests.groovy
@@ -0,0 +1,17 @@
+package gracker
+
+import grails.test.*
+
+class CrumbServiceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
