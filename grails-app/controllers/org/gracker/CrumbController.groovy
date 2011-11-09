5331e15adc39c36b45937eefe901ebf61758788f
filip <haase.filip@googlemail.com>
2011-11-10 00:21:40 +0100

    Scheduler + DatenAPI

--- grails-app/controllers/org/gracker/CrumbController.groovy
+++ grails-app/controllers/org/gracker/CrumbController.groovy
@@ -1,7 +1,5 @@
 package org.gracker
 
-import grails.converters.JSON
-
 class CrumbController {
 
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
@@ -20,37 +18,16 @@ class CrumbController {
 	
 	def showResults = {
 		if(params.id){
-			def jobs = Job.findAllByCrumb(Crumb.get(params.id))
-			ArrayList<Object> result = new ArrayList<Object>()
-
-			//Counter for the job Numbers
-			int counter = 0
-
-			//Iterate over all the Jobs
-			for(Job j: jobs){
-				//Get the Result String 
-				def mapString = j.result
-				
-				mapString = mapString.replace(']', '}')
-				mapString = mapString.replace('[', '{')
-				if(mapString != "{:}"){
-					//Parse the String
-					def parseResult = (JSON.parse(mapString))
-					
-					//Make a new Entry in the result List with a new List where we write in all the Components of the Job
-					result[counter] = new ArrayList<Object>()
-					result[counter][0] = j.dateCreated
-					
-					for(def k : parseResult.keySet())
-						result[counter][new Integer(k)]= parseResult[k].toString()
-						
-					counter++
-				}else{
-					result[counter] = new ArrayList<Object>()
-					result[counter][0] = j.dateCreated 
-				}
-			}	
-			[jobList: result, crumbName: Crumb.get(params.id).name]
+			try{
+				[
+					jobList: crumbService.getDataArray(params.id), 
+					attNames: crumbService.getAttributNames(params.id), 
+					attTypes: crumbService.getAttributTypes(params.id), 
+					crumbName: Crumb.get(params.id).name
+				]
+			}catch(Exception e){
+				flash.message = e.message
+			}
 		}else{
 			flash.message = "Crumb with id " + params.id + " was not found."
 			redirect(action: list)
--- grails-app/services/org/gracker/CrumbService.groovy
+++ grails-app/services/org/gracker/CrumbService.groovy
@@ -1,5 +1,6 @@
 package org.gracker
 
+import grails.converters.JSON
 
 class CrumbService {
 
@@ -27,5 +28,113 @@ class CrumbService {
 			}
 			
 			return regexMap.toMapString()	
+		}
+		
+		def getDataArray(def id){
+			if(id && Crumb.get(id)){
+				def jobs = Job.findAllByCrumb(Crumb.get(id))
+				ArrayList<Object> result = new ArrayList<Object>()
+	
+				//Counter for the job Numbers
+				int counter = 0
+	
+				//Iterate over all the Jobs
+				for(Job j: jobs){
+					//Get the Result String
+					def mapString = j.result
+					
+					mapString = mapString.replace(']', '}')
+					mapString = mapString.replace('[', '{')
+					if(mapString != "{:}"){
+						//Parse the String
+						def parseResult = (JSON.parse(mapString))
+						
+						//Make a new Entry in the result List with a new List where we write in all the Components of the Job
+						result[counter] = new ArrayList<Object>()
+						result[counter][0] = j.dateCreated
+						
+						for(def k : parseResult.keySet())
+							result[counter][new Integer(k)]= parseResult[k].toString()
+							
+					}else{
+						result[counter] = new ArrayList<Object>()
+						result[counter][0] = j.dateCreated
+
+						for(int i=1; i <= getAttributCount(id); i++)
+							result[counter][i] = 0
+					}
+					counter++
+				}
+				return result
+
+			}else{
+				throw new Exception("Crumb Not Found")
+			}
+		}
+		
+		def getAttributNames(def id){
+			if(id && Crumb.get(id)){
+				def aMS = Crumb.get(id).attributesMapString
+				aMS = aMS.replace(']', '}')
+				aMS = aMS.replace('[', '{')
+				
+				ArrayList<Object> result = new ArrayList<Object>()
+				int counter = 0
+
+				if(aMS != "{:}" && !aMS.isEmpty() && aMS){
+					//Parse the AttributeMapString
+					def parseResult = (JSON.parse(aMS))
+
+					//Iterate over the Attributes
+					for(def att : parseResult.keySet()){
+						result[counter] = parseResult[att]['Name']
+						counter++
+					}
+					return result.reverse()
+				}else return null
+	
+			}else{
+				throw new Exception("Crumb Not Found")
+			}
+		}
+		
+		def getAttributTypes(def id){
+			if(id && Crumb.get(id)){
+				def aMS = Crumb.get(id).attributesMapString
+				aMS = aMS.replace(']', '}')
+				aMS = aMS.replace('[', '{')
+				
+				ArrayList<Object> result = new ArrayList<Object>()
+				int counter = 0
+
+				if(aMS != "{:}" && !aMS.isEmpty() && aMS){
+					//Parse the AttributeMapString
+					def parseResult = (JSON.parse(aMS))
+
+					//Iterate over the Attributes
+					for(def att : parseResult.keySet()){
+						result[counter] = parseResult[att]['Type']
+						counter++
+					}
+					return result.reverse()
+				}else return null
+	
+			}else{
+				throw new Exception("Crumb Not Found")
+			}
+		}
+		
+		def getAttributCount(def id){
+			if(id && Crumb.get(id)){
+				def aMS = Crumb.get(id).attributesMapString
+				aMS = aMS.replace(']', '}')
+				aMS = aMS.replace('[', '{')
+				
+				if(aMS && !aMS?.isEmpty() && aMS != "{:}")	(JSON.parse(aMS)).keySet().size()
+				else 				0
+				
+			}else{
+				throw new Exception("Crumb Not Found")
+			}
 		}
 }
